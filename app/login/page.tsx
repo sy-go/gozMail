53f8ce988a5cd129959420c9243101f825abbc77
go-sy <sylvek2407@gmail.com>
2025-07-02 21:10:41 +0100

    login middleware deployment .3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,28 +5,32 @@ import LoginForm from '@/app/ui/userLogIn';
 import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-export default function LoginPage() {
+function LoginPageContent() {
   const searchParams = useSearchParams();
   const success = searchParams.get('success');
   const user = searchParams.get('user');
 
+  return (
+    <>
+      {success && user && (
+        <div className="alert alert-success">
+          User <b>{user}</b> successfully registered! Please log in.
+        </div>
+      )}
+      <LoginForm />
+    </>
+  );
+}
 
-
+export default function LoginPage() {
   return (
     <div className='m-1 md:m-4 mt-4'>
       <Link href='/' className='border rounded-md p-2 m-2'>Main</Link>
-      <Link
-        href='/register'
-        className="border rounded-md p-2 m-2">Register</Link>
-      <div className="flex items-center justify-center  min-h-screen m-0.5">
+      <Link href='/register' className="border rounded-md p-2 m-2">Register</Link>
+      <div className="flex items-center justify-center min-h-screen m-0.5">
         <div className='w-full md:w-2/6'>
-          {success && user && (
-            <div className="alert alert-success">
-              User <b>{user}</b> successfully registered! Please log in.
-            </div>
-          )}
-          <Suspense>
-            <LoginForm />
+          <Suspense fallback={<div>Loading...</div>}>
+            <LoginPageContent />
           </Suspense>
         </div>
       </div>
